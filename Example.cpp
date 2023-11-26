78548b391ca7f38ebc092e2722071fb006ee49ae
Raul Ernesto Guillen Hernandez <00012119@uca.edu.sv>
2023-11-26 14:03:16 -0600

    Still left Uses for Struct and variable alone asigment

--- Example.cpp
+++ Example.cpp
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
-int add(int a, int b) {
-    return a + b;
+int add( int a, int b ) {
+    int a = 2 * a ;
+    int b = 3 * b ;
+    if ( ID > ID ) {
+        ID = ID + ID ;
+    } else {
+        ID = ID + ID ;
+    }
+    return ID + ID ;
 }
 
 struct Point {
     int x;
     int y;
 };
-
+int ID = ID ;
 int main() {
     // Declaración de variables (VarDec)
     struct Point point1;
@@ -33,38 +40,21 @@ int main() {
 
 /*
 
-#include <stdio.h>
-
-// Declaración de una estructura (TypeDec)
-struct Point {
-    int x;
-    int y;
-};
-
-// Declaración de una función (FunctionDec)
-int add(int a, int b) {
-    return a + b;
-}
-
-int main() {
-    // Declaración de variables (VarDec)
-    struct Point point1;
-    int result;
-    int a = 5;
-    int b = 3;
-
-    // Asignación y llamada a función (StatementRest)
-    point1.x = 10;
-    point1.y = 20;
-    result = add(a, b);
-
-    // Bucle while (WhileStatement)
-    while (result > 0) {
-        printf("Resultado: %d\n", result);
-        result--;
+int ID() {
+    int ID = ID;
+    int ID = ID ;
+    int ID = ID ;
+    ID.ID = ID;
+    ID.ID = ID;
+    ID = ID( ID , ID ) ;
+    ID ID ;
+    while ( ID > ID ) {
+        cout << ID << endl ;
+        ID = ID - 1 ;
     }
 
-    return 0;
+    return ID ;
 }
 
+PREPROCESSOR < ID > int ID ( int ID , int ID ) { ID = ID * ID ; ID = ID * ID ; if ( ID > ID ) { ID = ID + ID ; } else { ID = ID + ID ; } return ID + ID ; } struct ID { int ID ; int ID ; } ; int ID = ID ;
 */
