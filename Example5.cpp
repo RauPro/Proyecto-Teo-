ee84f79f5e65359780d124c3e1d664227f958e5e
Henry Cortez <henry.cortez@airlinemro.parts>
2023-12-04 16:55:27 -0600

    :sparkles: Add c examples

---
+++ Example5.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+struct Point {
+    int x;
+    int y;
+    int a;
+    int c;
+    int b;
+};
+
+int main() {
+    int result = 15;
+    int a = 5;
+    int b = 3;
+
+    // Asignación y llamada a función (StatementRest)
+    point1.x = 10;
+    point1.y = 20;
+    while (result > 0) {
+        add(5,10)
+    }
+
+    return 0;
+}
